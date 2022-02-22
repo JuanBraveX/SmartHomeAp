355205418bb96110092937c6954965d11ef03778
JuanBraveX <91648724+JuanBraveX@users.noreply.github.com>
2022-02-22 14:22:17 -0600

    Initial commit

---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthomeap/Config.java
@@ -0,0 +1,5 @@
+package mx.tecnm.cdhidalgo.smarthomeap;
+
+public class Config {
+    public static final String URL ="http://172.17.123.57/smarthome/";
+}
---
+++ app/src/main/java/mx/tecnm/cdhidalgo/smarthomeap/MySingleton.java
@@ -0,0 +1,5 @@
+package mx.tecnm.cdhidalgo.smarthomeap;
+
+public class MySingleton {
+    
+}
